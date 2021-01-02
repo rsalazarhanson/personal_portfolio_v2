cab4e54508e9924915444c9e0c91c466d58bedb1
rsalazarhanson <rsalazarhanson@gmail.com>
2021-01-02 11:10:48 -0300

    blog model and more

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.TextField(max_length=250)
+    body = models.TextField(max_length=2500)
